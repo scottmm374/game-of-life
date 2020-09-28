cfe827a386e7639fa4049382077cc4db99a6f277
Michelle Scott <scottmm374@gmail.com>
2020-09-28 16:34:53 -0400

    code cleanup in app.js

--- src/App.js
+++ src/App.js
@@ -2,18 +2,12 @@ import React from "react";
 import "./App.css";
 import ControlPanel from "./components/ControlPanel";
 import gameOfLife from "./images/gameOfLife.png";
-import styled from "styled-components";
-
-const HeadImg = styled.img`
-  width: 40%;
-  box-shadow: 13px 15px 9px #0000008c;
-`;
 
 function App() {
   return (
     <div className="App">
       <header>
-        <HeadImg id="header-img" src={gameOfLife} alt="gameOfLife" />
+        <img id="header-img" src={gameOfLife} alt="gameOfLife" />
       </header>
 
       <ControlPanel />
