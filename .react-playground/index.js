48bfa64eca4912ebad7bfa8c595d8eb103ccf9f6
Michelle Scott <scottmm374@gmail.com>
2020-09-27 22:05:07 -0400

    removed google font not needed in index.html

--- .react-playground/index.js
+++
@@ -1,8 +0,0 @@
-
-        import React from 'react';
-        import ReactDom from 'react-dom';
-        import Playground from 'react-playground-kit';
-        import * as module from '/Users/michellescott/Documents/Lambda/game-of-life/src/App.js';
-
-        ReactDom.render(React.createElement(Playground, { module }), document.getElementById('app'));
-    
