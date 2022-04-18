ef5f9f50cb4758d85f75ee3c622eee1468dbce43
Michelle <scottmm374@gmail.com>
2022-04-18 12:40:02 -0400

    added control icons

--- src/components/views/ControlView.js
+++ src/components/views/ControlView.js
@@ -5,15 +5,15 @@ const ControlView = (props) => {
   return (
     <>
       <ButtonGroup>
-        <button className='control-button' onClick={props.startGame}>
-          Start
-        </button>
-        <button className='control-button' onClick={props.stopGame}>
+        <i class='fa-solid fa-circle-play' onClick={props.startGame}></i>
+        <i class='fa-solid fa-circle-pause' onClick={props.stopGame}></i>
+        <i class='fa-solid fa-arrows-rotate' onClick={props.clearBoard}></i>
+        {/* <button className='control-button' onClick={props.stopGame}>
           Stop
         </button>
         <button className='control-button' onClick={props.clearBoard}>
           Clear
-        </button>
+        </button> */}
       </ButtonGroup>
 
       {/* </Col>
