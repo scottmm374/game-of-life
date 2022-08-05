6d9cd7aee16aea4294e399ac49acced2647fddf1
Michael Williams <williamsmikeyj@outlook.com>
2022-08-05 13:49:47 -0400

    add bootstrap link to App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import ControlPanel from './components/ControlPanel';
 import Header from './containers/Header';
