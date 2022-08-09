1e134e70d42c6766f6f83d9dff90b90ac8819551
Michael Williams <williamsmikeyj@outlook.com>
2022-08-09 12:17:22 -0400

    rm divs. add rows & cols for responsiveness sizing next phase

--- src/components/views/PresetView.js
+++ src/components/views/PresetView.js
@@ -8,39 +8,42 @@ const PresetView = (props) => {
       <Container>
         <Row>
           <Col sm='12'>
-            {/* <div> */}
             <h4 className='titles-preset-rules'>Choose a Preset</h4>
-            {/* </div> */}
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <div className='button-containers'>
+              <ButtonGroup>
+
+                <button
+                  className='preset-button'
+                  onClick={props.handlePresets}
+                  value='thunderbird'
+                >
+                  Thunderbird
+                </button>
+
+                <button
+                  button
+                  className='preset-button'
+                  onClick={props.handlePresets}
+                  value='quad_thunderbird'
+                >
+                  Quad-Thunderbird
+                </button>
+                <button
+                  className='preset-button'
+                  onClick={props.handlePresets}
+                  value='infinite'
+                >
+                  Infinite
+                </button>
+              </ButtonGroup>
+            </div>
           </Col>
         </Row>
 
-        <div className='button-containers'>
-          <ButtonGroup>
-            <button
-              className='preset-button'
-              onClick={props.handlePresets}
-              value='thunderbird'
-            >
-              Thunderbird
-            </button>
-
-            <button
-              button
-              className='preset-button'
-              onClick={props.handlePresets}
-              value='quad_thunderbird'
-            >
-              Quad-Thunderbird
-            </button>
-            <button
-              className='preset-button'
-              onClick={props.handlePresets}
-              value='infinite'
-            >
-              Infinite
-            </button>
-          </ButtonGroup>
-        </div>
         <div className='button-containers'>
           <ButtonGroup>
             <button
